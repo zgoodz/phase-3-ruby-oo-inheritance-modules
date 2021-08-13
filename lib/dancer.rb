aab4b7f101832a4fa2550a8df41418075e666f0f
Zachary Goodman <znote89@gmail.com>
2021-08-13 12:29:06 -0400

    commit tree 824ffb5027000cc7ccd776fcc6936a45f347c424

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,12 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+
+class Kid
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
